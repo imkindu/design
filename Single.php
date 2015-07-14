51ea6eaa104baba7924c2334c020cbec3483c322
imkindu <531694409@qq.com>
2015-07-14 17:00:34 +0800

    单例模式 注释

--- Single.php
+++ Single.php
@@ -2,19 +2,25 @@
 
 /*
 *	单例模式
+*   1、普通类
+*   2、封锁new操作
+*   3、六个接口来new对象
+*   4、先判断后实例对象
 *	5、用final，防止继承时。被修改权限
 * 	6、禁止克隆clone
 */
 
+header('Content-Type: text/html; charset=utf-8');
+
 class Single
 {
-	private $_obj = null;		//私有属性，保存对象
+	public static $obj = null;		//私有属性，保存对象
 	
     public static function getIns(){
-    	if($this->_obj == null){
-    		$this->_obj == new self();
+    	if(self::$obj == null){
+    		self::$obj == new self();
     	}
-    	return $this->_obj;
+    	return self::$obj;
     }
 
     //方法前用final，则方法不能被覆盖，
@@ -29,13 +35,13 @@ class Single
     }
 }
 
-$s1 = new Single();
-$s2 = Single::getIns();
+//$s1 = new Single();
+$s1 = Single::getIns();
 $s2 = Single::getIns();
 
-$s3 = clone $s1;
+//$s3 = clone $s2;
 if($s1 === $s2){
-	echo '是一个对象'；
+	echo '是一个对象';
 }else{
 	echo "不是一个对象";
 }
